a9fd99dd2848f0e456b7853bc9f8f6ad9d52da19
Josmar Marroquin <josmarmp96@gmail.com>
2023-01-08 11:37:53 -0600

    Remove service

--- lib/pages/credit_card.dart
+++ lib/pages/credit_card.dart
@@ -12,47 +12,38 @@ class CreditCardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final PaymentBloc paymentBloc = BlocProvider.of<PaymentBloc>(context);
-
-    final CreditCardModelCustom creditCard = CreditCardModelCustom(
-      cardNumberHidden: '4242',
-      cardNumber: '4242424242424242',
-      brand: 'visa',
-      cvv: '213',
-      expiracyDate: '01/25',
-      cardHolderName: 'Josmar Salvador'
-    );
+    final CreditCardModelCustom creditCard =
+        context.read<PaymentBloc>().state.creditCard!;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Checkout with credit card'),
-        leading: IconButton(
-          onPressed: () {
-            paymentBloc.add(OnDeselectCreditCard());
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.chevron_left),
-        ),
-      ),
-      body: Column(
-        children: <Widget>[
-          Hero(
-            tag: creditCard.cardNumber,
-            child: CreditCardWidget(
-              cardNumber: creditCard.cardNumber,
-              expiryDate: creditCard.expiracyDate,
-              cardHolderName: creditCard.cardHolderName,
-              isHolderNameVisible: true,
-              cvvCode: creditCard.cvv,
-              showBackView: false,
-              onCreditCardWidgetChange: (_) {},
-              cardBgColor: Colors.black,
-            ),
+        appBar: AppBar(
+          title: const Text('Checkout with credit card'),
+          leading: IconButton(
+            onPressed: () {
+              context.read<PaymentBloc>().add(OnDeselectCreditCard());
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back_ios),
           ),
-          const Spacer(),
-          const Resume()
-        ],
-      )
-    );
+        ),
+        body: Column(
+          children: <Widget>[
+            Hero(
+              tag: creditCard.cardNumber,
+              child: CreditCardWidget(
+                cardNumber: creditCard.cardNumber,
+                expiryDate: creditCard.expiracyDate,
+                cardHolderName: creditCard.cardHolderName,
+                isHolderNameVisible: true,
+                cvvCode: creditCard.cvv,
+                showBackView: false,
+                onCreditCardWidgetChange: (_) {},
+                cardBgColor: Colors.black,
+              ),
+            ),
+            const Spacer(),
+            const Resume()
+          ],
+        ));
   }
 }
